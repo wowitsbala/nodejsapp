76066656cbfe81cce7f28142c91147a964f060b0
wowitsbala <88251527+wowitsbala@users.noreply.github.com>
2025-07-10 10:47:29 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./index');
-   const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-   app.listen(port, () => {
-     console.log(`Server running on port ${port}`);
-   });
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
